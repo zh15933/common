7bc9ea9efd23bc172e3cde37dbc67c57f4066e72
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-10-19 23:28:42 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -140,14 +140,14 @@ elif [[ "${REPO_BRANCH}" == "master" ]]; then
   echo "LUCI_EDITION=18.06" >> $GITHUB_ENV
   echo "MAINTAIN=Lean's" >> $GITHUB_ENV  
   
-elif [[ "${REPO_BRANCH}" == "22.03" ]]; then
+elif [[ "${REPO_BRANCH}" == "21.02" ]]; then
   echo "ZZZ_PATH=${GITHUB_WORKSPACE}/openwrt/package/default-settings/files/zzz-default-settings" >> $GITHUB_ENV
   if [[ ! -f "${GITHUB_WORKSPACE}/openwrt/package/default-settings/files/zzz-default-settings" ]]; then
     TIME r "上游源码作者修改了zzz-default-settings文件的路径或者名称，找编译脚本的作者及时修改"
     exit 1
   fi
   echo "SOURCE=Lienol" >> $GITHUB_ENV
-  echo "LUCI_EDITION=22.03" >> $GITHUB_ENV
+  echo "LUCI_EDITION=21.02" >> $GITHUB_ENV
   echo "MAINTAIN=Lienol's" >> $GITHUB_ENV
 elif [[ "${REPO_BRANCH}" == "openwrt-18.06" ]]; then
   echo "ZZZ_PATH=${GITHUB_WORKSPACE}/openwrt/package/emortal/default-settings/files/99-default-settings" >> $GITHUB_ENV
