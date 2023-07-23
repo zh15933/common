a919f59b4323532dbde016bf27e7774cafa2bbf5
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-07-24 05:20:51 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -271,6 +271,7 @@ EOF
 cat >>"feeds.conf.default" <<-EOF
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git passwall3 https://github.com/xiaorouji/openwrt-passwall.git;packages
+src-git  netdata https://github.com/sirpdboy/luci-app-netdata.git
 EOF
 
 App_path="$(find . -type d -name "applications" |grep 'luci' |sed "s?.?${HOME_PATH}?" |awk 'END {print}')"
