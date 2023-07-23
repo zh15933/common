1085e4ce07a5151d7d0f948ed7abe3718d017d8c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2023-07-23 19:44:16 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -265,7 +265,7 @@ mv -f uniq.conf feeds.conf.default
 
 # 这里增加了源,要对应的删除/etc/opkg/distfeeds.conf插件源
 cat >>"feeds.conf.default" <<-EOF
-src-git danshui1 https://github.com/281677160/openwrt-package.git;${SOURCE}
+src-git shidahuilang1 https://github.com/shidahuilang/openwrt-package.git;${SOURCE}
 EOF
 ./scripts/feeds update -a
 cat >>"feeds.conf.default" <<-EOF
