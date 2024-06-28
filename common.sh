0684790084ee894e201c4ebf1eda2eceb4de8e89
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2024-06-28 15:34:46 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -454,8 +454,10 @@ for X in $(ls -1 "${HOME_PATH}/feeds/passwall3"); do
 done
 # 删除软件包自带插件
 rm -rf feeds/packages/net/softethervpn5
-#rm -rf feeds/packages/net/cloudflared
+rm -rf feeds/packages/net/cloudflared
 rm -rf feeds/package/luci-app-smartdns
+rm -rf feeds/package/luci-app-zerotier
+
 # 更换golang版本
 rm -rf ${HOME_PATH}/feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x ${HOME_PATH}/feeds/packages/lang/golang
